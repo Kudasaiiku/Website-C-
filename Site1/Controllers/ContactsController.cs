d5601db539e26e3e1856e89aacc7bf7d21bb88c6
Lana <sveta0ka@yandex.ru>
2024-06-19 21:17:23 +0300

    Website-CSharp
    Website-CSharp

---
+++ Site1/Controllers/ContactsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Site1.Models;
+
+namespace Site1.Controllers
+{
+	public class ContactsController : Controller
+	{
+		public IActionResult Index() // Contacts
+		{
+			return View();
+		}
+
+		[HttpPost]
+        public IActionResult Check(Contacts contact) // Check
+        {
+            if(ModelState.IsValid)
+			{
+				return Redirect("/");
+            }
+			else
+			{
+                return View("Index");
+            }	
+        }
+    }
+}
---
+++ Site1/Models/Contacts.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Site1.Models
+{
+    public class Contacts
+    {
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Обязательно введите имя!")]
+        public string Name { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Обязательно введите фамилию!")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Возраст")]
+        [Required(ErrorMessage = "Обязательно введите возраст!")]
+        public int Age { get; set; }
+
+        [Display(Name = "Электронная почта")]
+        [Required(ErrorMessage = "Обязательно введите электронную почту!")]
+        public string Email { get; set; }
+
+        [Display(Name = "Сообщение")]
+        [Required(ErrorMessage = "Обязательно введите сообщение!")]
+        [StringLength(30, ErrorMessage = "Текст менее 30 символов!")]
+        public string Message { get; set; }
+
+        [Display(Name = "Смайлик!")]
+        [Required(ErrorMessage = "Обязательно введите смайлик!")]
+        public string Emoji { get; set; }
+    }
+}
